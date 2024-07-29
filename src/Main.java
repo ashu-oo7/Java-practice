7cea4cb39c833abcb9ee66790d0c9bfa22a70736
ashu-oo7 <ashu0705000@gmail.com>
2024-07-30 02:15:53 +0530

    adding files

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+interface automobiles{
+    public void add_gear(int val);
+}
+class car implements automobiles{
+    int gear = 0;
+    public void add_gear(int val){
+        gear += val;
+        System.out.println(gear);
+    }
+}
+
+public class Main{
+
+    public static void main(String[] args) {
+        car obj = new car();
+        obj.add_gear(5);
+        System.out.println("Hello world!");
+    }
+}
